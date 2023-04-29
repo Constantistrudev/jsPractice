c54a9adf73c4176ae2b7be87c77661e3b8b39225
Constantistrudev <79713751+Constantistrudev@users.noreply.github.com>
2023-04-29 23:55:34 +0300

    Вынесение расчетов в функции, добавление заглушек для будующего функционала v1.2

--- JS/figureCalc.js
+++ JS/figureCalc.js
@@ -1,38 +1,53 @@
 let flag = true;
 const PI = 3.14;
-while(flag){
-    let cicle = parseInt(prompt("Количество сторон",''));
-    if(cicle ==0){
-        let radius = parseInt(prompt("Введите радиус",''));
-        alert(`Круг\n Радиус = ${radius} \n Площадь = ${radius*(PI**2)} \n Длина окружности = ${radius*2*PI}`)
-    }
-    // else if (cicle ==3){
-    //     /*
-    //     провекрка на существование треугольника
-    //         поиск площади, периметра, углов, высоты
-    //     вывод
-    //     */
-    //     let arr=[];
-    //     for(let i=0;i<cicle;i++){
-    //         arr[i]= parseInt(prompt("Длина стороны. Целое число",0));
-    //     }
 
-    // }
-    else if (cicle >=3){
+// Что за фигура
+function shapeDefinition(sidesCount){
+    return ''
+}
+// Проверка на существование треугольника
+function isTriangle(sidesArray){
+    return true
+}
+//Периметр
+function perimetrCalc(sidesArray){
+    let perimetr = 0;
+    for(let i =0; i<sidesArray.length;i++){
+        perimetr+=sidesArray[i];
+    }
+    return perimetr
+}
+//Площадь
+function squareCalc(sidesArray){
+    let square = 1;
+    for(let i =0; i<sidesArray.length;i++){
+        square*=sidesArray[i]**0.5;
+    }
+    return square.toFixed(2)
+}
+//Углы
+function angleCalc(sidesArray){
+    //провести подстчет по треугольникам.
+}
+
+while(flag){
+    let sides = parseInt(prompt("Количество сторон",''));
+    let shape = shapeDefinition(sides);
+    if(sides == 0){
+        let radius = parseInt(prompt("Введите радиус",''));
+        alert(`${shape}\n Радиус = ${radius} \n Площадь = ${radius*(PI**2)} \n Длина окружности = ${radius*2*PI}`)
+    }
+    else if (sides >=3){
         let arr=[];
-        for(let i=0;i<cicle;i++){
-            arr[i]= parseInt(prompt("Длина стороны. Целое число",0));
+        for(let i=0;i<sides;i++){
+            arr[i]= Math.abs(parseInt(prompt("Длина стороны. Целое число",0)));
         }
         if(arr.indexOf(0)==-1){
-            let perimetr = 0;
-            for(let i =0; i<arr.length;i++){
-                perimetr+=arr[i];
+            if(isTriangle(arr)){
+                angleCalc(arr)
             }
-            let square = 1;
-            for(let i =0; i<arr.length;i++){
-                square*=arr[i]**0.5;
-            }
-            alert(`Периметр = ${perimetr} \n Площадь = ${square.toFixed(2)}`)
+
+            alert(`Периметр = ${perimetrCalc(arr)} \n Площадь = ${squareCalc(arr)}`)
         }
         else{
             alert('Такой фигуры нету');
