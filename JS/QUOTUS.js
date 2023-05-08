1652c1dc76f881814a26436b5256ec180b51315c
Constantistrudev <79713751+Constantistrudev@users.noreply.github.com>
2023-05-08 23:25:37 +0300

    Добавление цитат. Проблема с подключением АПИ v3.0

---
+++ JS/QUOTUS.js
@@ -0,0 +1,19 @@
+let theme = ['age','alone','amazing' ,'anger','architecture','art','attitude','beauty','best','birthday','business','car','change','communications','computers','cool','courage','dad','dating','death','design','dreams','education','environmental','equality','experience','failure','faith','family','famous','fear','fitness','food','forgiveness','freedom','friendship','funny','future','god','good','government','graduation','great','happiness','health','history','home','hope','humor','imagination','inspirational','intelligence','jealousy','knowledge','leadership','learning','legal','life','love','marriage','medical','men','mom','money','morning','movies','success']
+const KEY = '7y40ojUmEjAQKt1Zf09nHA==AsWfq5TBrq4zDTyy'
+let button = document.querySelector('.button')
+let $;
+
+button.addEventListener('click', function (){
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/quotes?category=' + theme[1],
+        headers: { 'X-Api-Key': KEY},
+        contentType: 'application/json',
+        success: function(result) {
+            console.log(result);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+})
