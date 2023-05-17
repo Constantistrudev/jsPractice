809cf8d66e41a6cd61e57aa40049fe84d4b79bf5
Constantistrudev <79713751+Constantistrudev@users.noreply.github.com>
2023-05-17 12:13:50 +0300

    Добавлены функции перезапуска игры и ответов v5.2

--- JS/GuesNumberGame.js
+++ JS/GuesNumberGame.js
@@ -3,6 +3,7 @@ let containerButton = document.querySelector('.container__button');
 let messageText = document.querySelector('.container__message-text');
 let count = document.getElementById("count");
 let gues = document.getElementById("gues");
+let number =Math.floor(Math.random()*100)+1;
 let answers = [];
 containerButton.addEventListener('click', () => 
 {
@@ -11,24 +12,32 @@ containerButton.addEventListener('click', () =>
     }
     else{
         count.innerHTML = ++count.innerHTML;
-        let number =Math.floor(Math.random()*100);
+
         if( number != containerInput.value ){
-            if(answers.indexOf(containerInput.value) == -1)
+            if(parseInt(answers.indexOf(containerInput.textContent)) == -1)
             {
             answers.push(parseInt(containerInput.value));
-            gues.innerHTML += `${answers}, `;
+            console.log(answers);
+            gues.innerHTML = `${answers}, `;
             }
-
             if(number > containerInput.value){
+                messageText.innerHTML="Wrong. Number is higher";
+                messageText.classList.remove('none');
+                messageText.classList.add('wrong');
             }
             else {
-                
+                messageText.innerHTML="Wrong. Number is lower";
+                messageText.classList.remove('none');
+                messageText.classList.add('wrong');
             }
         }
         else{
             messageText.innerHTML="Correct"
             messageText.classList.remove('none');
+            messageText.classList.remove('wrong');
             messageText.classList.add('correct');
+            setTimeout(()=>location.reload(),2000);
+
         }  
     }  
 });
